037b52753eea487a9e5ade4a172cf2cf8a01a83f
sumansekhar-code <rajameher111@gmail.com>
2023-03-07 10:26:10 +0530

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # test
+This is my first Data science project.
+
+It describes the whole life cycle of a data science project.
+    -The 1st stage is collecting/gathering data from different sources.
--- scrapping.py
+++ scrapping.py
@@ -3,6 +3,6 @@ import pandas as pd
 
 path = "D:/DS/proj-1/chromedriver.exe"
 
-df = gs.get_jobs('data scientist',100 , False, path, 15)
-df
+df = gs.get_jobs('Web Developer',30 , False, path, 15)
+#df
 df.to_csv('glassdoor_jobs.csv', index = False)
